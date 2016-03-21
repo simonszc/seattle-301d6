126065ce4687f9a6627719f1cbd0045cea70761c
Zach Simonson <zsimonson26@gmail.com>
2016-03-21 13:04:35 -0700

    fixed typo that broke stuff

--- class-11-spa-routing/pair-assignment/zach-susan/starter-code/scripts/routes.js
+++ class-11-spa-routing/pair-assignment/zach-susan/starter-code/scripts/routes.js
@@ -1,6 +1,6 @@
 // DONE: Configure our two routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it:
-page('/', articleController.index);
+page('/', articlesController.index);
 page('/about', aboutController.index);
 
 // DONE: What function do we call to activate page.js? Fire it off now, to execute
