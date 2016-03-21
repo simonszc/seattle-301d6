73e4526f53da8d719b3ad4f9d0f3bb3e15798111
Susan Harrison <susan77harrison@gmail.com>
2016-03-21 12:54:01 -0700

    Added express. and jQuery to hide and fadeIn sections.

--- class-11-spa-routing/pair-assignment/zach-susan/starter-code/scripts/articleController.js
+++ class-11-spa-routing/pair-assignment/zach-susan/starter-code/scripts/articleController.js
@@ -1,15 +1,16 @@
 (function(module) {
   var articlesController = {};
 
-  // dONE: Create the `articles` table when the controller first loads, with the code that used to be in index.html:
+  // DONE: Create the `articles` table when the controller first loads, with the code that used to be in index.html:
   Article.createTable();
 
   // DONE: Complete this function below that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
   Article.fetchAll(articleView.initIndexPage);
-  // TODO: Also be sure to hide all the main section elements, and reveal the #articles section:
+  // DONE: Also be sure to hide all the main section elements, and reveal the #articles section:
   articlesController.index = function() {
-
+    $('main > section').hide();
+    $('#articles').fadeIn();
 
   };
 
